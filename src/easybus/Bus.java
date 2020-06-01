de193159da41541bee9887f74b9cabf72b1add5a
Shmuel Azrad <shmuel010@gmail,com>
2020-06-01 18:58:18 +0300

    Shmuel

---
+++ src/easybus/Bus.java
@@ -0,0 +1,22 @@
+package easybus;
+
+public class Bus  extends Car
+{
+    int numOfSeat;
+
+    public Bus(int yearlyCost, int fuelConsumption, int licenseNum, String model, int year, int numOfSeat)
+    {
+        super(yearlyCost, fuelConsumption, licenseNum, model, year);
+        this.numOfSeat = numOfSeat;
+    }
+
+    public int getNumOfSeat()
+    {
+        return numOfSeat;
+    }
+
+    public void setNumOfSeat(int numOfSeat)
+    {
+        this.numOfSeat = numOfSeat;
+    }
+}
---
+++ src/easybus/Minibus.java
@@ -0,0 +1,21 @@
+package easybus;
+
+public class Minibus
+{
+    int numOfSeat;
+
+    public Minibus(int numOfSeat)
+    {
+        this.numOfSeat = numOfSeat;
+    }
+
+    public int getNumOfSeat()
+    {
+        return numOfSeat;
+    }
+
+    public void setNumOfSeat(int numOfSeat)
+    {
+        this.numOfSeat = numOfSeat;
+    }
+}
