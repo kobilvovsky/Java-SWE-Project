96befdf685d91086f2ac08be95466c73b5ffe810
Erez Mizrahi <erezmiz@my.hit.ac.il>
2020-07-02 16:15:03 +0300

    test

--- src/easybus/main.java
+++ src/easybus/main.java
@@ -1,4 +1,5 @@
 package easybus;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,11 +7,86 @@ import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class main
 {
-    public static void main(String[] args) throws IOException
+    private JFrame mainFrame;
+    private JLabel headerLabel;
+    private JLabel statusLabel;
+    private JPanel controlPanel;
+
+    public main(){
+        prepareGUI();
+    }
+
+    private void prepareGUI(){
+        mainFrame = new JFrame("Java SWING Examples");
+        mainFrame.setSize(1042,842);
+        mainFrame.setLayout(new GridLayout(5, 1));
+
+        headerLabel = new JLabel("",JLabel.CENTER );
+        statusLabel = new JLabel("",JLabel.CENTER);
+        statusLabel.setSize(350,100);
+
+        mainFrame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent windowEvent){
+                System.exit(0);
+            }
+        });
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+
+        mainFrame.add(headerLabel);
+        mainFrame.add(controlPanel);
+        mainFrame.add(statusLabel);
+        mainFrame.setVisible(true);
+    }
+
+    private void showEventDemo(){
+        headerLabel.setText("Control in action: Button");
+
+        JButton okButton = new JButton("OK");
+        JButton submitButton = new JButton("Submit");
+        JButton cancelButton = new JButton("Cancel");
+
+        okButton.setActionCommand("OK");
+        submitButton.setActionCommand("Submit");
+        cancelButton.setActionCommand("Cancel");
+
+        okButton.addActionListener(new ButtonClickListener());
+        submitButton.addActionListener(new ButtonClickListener());
+        cancelButton.addActionListener(new ButtonClickListener());
+
+        controlPanel.add(okButton);
+        controlPanel.add(submitButton);
+        controlPanel.add(cancelButton);
+
+        mainFrame.setVisible(true);
+    }
+
+    private class ButtonClickListener implements ActionListener{
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+
+            if( command.equals( "OK" ))  {
+                statusLabel.setText("Ok Button clicked.");
+            } else if( command.equals( "Submit" ) )  {
+                statusLabel.setText("Submit Button clicked.");
+            } else {
+                statusLabel.setText("Cancel Button clicked.");
+            }
+        }
+    }
+
+    public static void main(String[] args) //throws IOException
     {
-        /* MENU */
+        main swingControlDemo = new main();
+        swingControlDemo.showEventDemo();
+
+        /*
         Dictionary<Integer, String> menuOption = new Hashtable<Integer, String>();
         menuOption.put(1, "Introduction");
         menuOption.put(2, "View Vehicles");
@@ -115,6 +191,6 @@ public class main
                     break;
                 }
             }
-        }
+        }*/
     }
 }
